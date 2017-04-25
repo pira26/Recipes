7e4a4a544d5ee6aeade4857a15b327d2fa93e254
Pira <sritharan.pirathaab@gmail.com>
2017-04-25 11:48:05 +0200

    App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import NavBar from './components/NavBar';
+import Home from './containers/Home';
+import Submit from './containers/Submit';
 
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <NavBar />
-      </div>
+      <Router>
+  		<div>
+  			<NavBar/>
+
+  			<Route exact path="/" component={NavBar}></Route>
+  			<Route path="/home" component={Home}></Route>
+  			<Route path="/submit" component={Submit}></Route>
+
+  		</div>
+  	</Router>
     );
   }
 }

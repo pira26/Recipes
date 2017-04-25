04e85a07c52349ae061293b7a2536755f4fc981e
Pira <sritharan.pirathaab@gmail.com>
2017-04-25 11:48:05 +0200

    submit

---
+++ src/containers/Submit.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class Submit extends Component {
+	render() {
+		return (
+			<div>
+    			<button type="submit">Submit</button>
+  			</div>
+
+		)
+	}
+}

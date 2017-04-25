bb52ffd8113cdfda2b827fb1e48c625dfc24b975
Pira <sritharan.pirathaab@gmail.com>
2017-04-25 11:48:05 +0200

    Home Page

---
+++ src/containers/Home.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+export default class Home extends Component {
+	render() {
+		return (
+			<div>
+				<ul>
+					<li>1</li>
+					<li>2</li>
+					<li>3</li>
+					<li>4</li>
+					<li>5</li>
+				</ul>
+    			<p>Home</p>
+  			</div>
+
+		)
+	}
+}
+  
+
